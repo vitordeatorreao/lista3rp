07f2fe5e621c863383900fdae7eb66247903e7f7
vitordeatorreao <vitordeatorreao@gmail.com>
2015-10-30 12:11:16 -0300

    Made the report

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dataaccess import DataSet
 from neuralnetworks import Perceptron
 from distbased import KNN
 
-def testPerceptron(filename):
+def testPerceptron(filename, alpha, nepochs):
 	from random import shuffle
 	from math import ceil
 	data = DataSet.readFromArff(filename)
@@ -12,7 +12,7 @@ def testPerceptron(filename):
 	shuffle(d)
 	ntrain = int(ceil(0.7*len(d)))
 	p = Perceptron(len(d[0])-1)
-	p.train(d[:ntrain], 0.3, 3000)
+	p.train(d[:ntrain], alpha, nepochs)
 	error_rate = 0
 	i = 0
 	for test in d[ntrain:len(d)]:
@@ -48,9 +48,17 @@ if __name__ == "__main__":
 	import sys
 	err = 0
 	for i in range(10):
-		err += testPerceptron(sys.argv[1])
+		try:
+			err += testPerceptron(sys.argv[1], float(sys.argv[2]), int(sys.argv[3])) \
+			 if len(sys.argv) > 3 else testPerceptron(sys.argv[1], 0.3, 1000)
+		except ValueError:
+			err += testPerceptron(sys.argv[1], 0.3, 1000)
 	print("Perceptron = ", err/10)
 	err = 0
 	for i in range(10):
-		err += testKNN(sys.argv[1], 10)
+		try:
+			err += testKNN(sys.argv[1], int(sys.argv[4])) \
+			 if len(sys.argv) > 4 else testKNN(sys.argv[1], 10)
+		except ValueError:
+			err += testKNN(sys.argv[1], 10)
 	print("KNN = ", err/10)
